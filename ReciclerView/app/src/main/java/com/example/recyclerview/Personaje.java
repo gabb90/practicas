c33e13e493088cff89baaac4590ba774d75cbd21
nicolas <lopez>
2019-05-27 21:15:26 -0300

    1

---
+++ ReciclerView/app/src/main/java/com/example/recyclerview/Personaje.java
@@ -0,0 +1,17 @@
+package com.example.recyclerview;
+
+public class Personaje {
+
+    private String nombre;
+    private Integer imagen;
+    private String habilidad;
+
+    public Personaje(String nombre, Integer imagen, String habilidad) {
+        this.nombre = nombre;
+        this.imagen = imagen;
+        this.habilidad = habilidad;
+    }
+
+    
+
+}
