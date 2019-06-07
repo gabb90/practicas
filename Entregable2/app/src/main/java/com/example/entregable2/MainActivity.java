7c97a642511739a559dc05839bcc594ff1859645
nicolas <lopez>
2019-06-07 20:19:15 -0300

    q

--- Entregable2/app/src/main/java/com/example/entregable2/MainActivity.java
+++ Entregable2/app/src/main/java/com/example/entregable2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.entregable2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
