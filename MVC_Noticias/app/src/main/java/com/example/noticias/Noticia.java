f8bffff3fcc810001abb803b105ccdc60e3cc824
nicolas <lopez>
2019-06-10 21:35:40 -0300

    q

---
+++ MVC_Noticias/app/src/main/java/com/example/noticias/Noticia.java
@@ -0,0 +1,13 @@
+package com.example.noticias;
+
+import java.util.Date;
+
+public class Noticia {
+    private String Titulo;
+    private Date Fecha;
+    private String Descripcion;
+    private String Autor;
+
+
+
+}
